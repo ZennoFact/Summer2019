fc827137048c660292ee60444081d20234aa5053
Kazuki Maeno <zenno04.g@gmail.com>
2019-07-29 11:09:10 +0900

    [fix]「何歳だからお酒が飲める」かを伝えたかったので，「○歳ならお酒が飲める」と修正

--- summer2019/Day1.java
+++ summer2019/Day1.java
@@ -7,9 +7,9 @@ public class Day1 {
 		// 成人ならお酒が飲めるという処理
 		int age = 20;
 		if (20 <= age) {
-			System.out.println("お酒が飲める！");
+			System.out.println(age + "歳ならお酒が飲める！");
 		} else {
-			System.out.println("お酒はまだ飲んではいけない！！");
+			System.out.println(age + "歳ならお酒はまだ飲んではいけない！！");
 		}
 	}
 }
