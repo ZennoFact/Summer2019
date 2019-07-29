a35713d6867e551c243f96b61b655e993dfabb33
Kazuki Maeno <zenno04.g@gmail.com>
2019-07-29 11:12:41 +0900

    [update]ユーザーの年齢入力を受け取るために，Scannerを追加

--- summer2019/Day1.java
+++ summer2019/Day1.java
@@ -1,15 +1,21 @@
 package summer2019;
 
+import java.util.Scanner;
+
 public class Day1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		
 		// 成人ならお酒が飲めるという処理
-		int age = 20;
+		Scanner scanner = new Scanner(System.in);
+		
+		int age = scanner.nextInt();
 		if (20 <= age) {
 			System.out.println(age + "歳ならお酒が飲める！");
 		} else {
 			System.out.println(age + "歳ならお酒はまだ飲んではいけない！！");
 		}
+		
+		scanner.close();
 	}
 }
