528e48cf0daa4de7f7be444debcd5ae3c1c3e05b
Kazuki Maeno <zenno04.g@gmail.com>
2019-07-29 11:04:58 +0900

    [update]if文を追加。20歳以上ならお酒が飲めるという処理

--- summer2019/Day1.java
+++ summer2019/Day1.java
@@ -3,5 +3,13 @@ package summer2019;
 public class Day1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		
+		// 成人ならお酒が飲めるという処理
+		int age = 20;
+		if (20 <= age) {
+			System.out.println("お酒が飲める！");
+		} else {
+			System.out.println("お酒はまだ飲んではいけない！！");
+		}
 	}
 }
